b7212f519b5ec102408e982b5b659677bb529ad5
tejaltanajisawale <tejal.tanajisawale@fiserv.com>
2026-02-12 19:06:08 +0530

    update

--- src/components/AppTabs.tsx
+++ src/components/AppTabs.tsx
@@ -30,9 +30,9 @@ export const AppTabs: React.FC = () => {
   const navigate = useNavigate();
 
   const tabRoutes: TabRoute[] = [
-    { label: "Components", path: "/basic", element: <Components /> },
+    { label: "Test Scorecard", path: "/basic", element: <Components /> },
     // { label: "Context", path: "/context", element: <PluginContext /> },
-    { label: "Test Scorecard", path: "/entity", element: <EntityDetails /> },
+    // { label: "Test Scorecard", path: "/entity", element: <EntityDetails /> },
     // { label: "Colors", path: "/colors", element: <ColorSwatches /> },
     // { label: "Proxy", path: "/proxy", element: <ProxyTest /> },
   ];
